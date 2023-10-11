8651ca2f143aaa913ed0cabfd545479566af67a5
Anurag Maldhure <hello.anurag.maldhure@gmail.com>
2023-10-11 22:10:07 +0530

    11 Oct

--- Google Demo Website/DragMeDown.js
+++ Google Demo Website/DragMeDown.js
@@ -1,14 +1,34 @@
 var leftMargin = 0;
-// var iterations = 0;
+var queueNosArray = new Array();
+var queueNo = window.setInterval(KeepCounting, 1000);
+queueNosArray.push(queueNo);
 
-function MarioMoveRight() {
+function GearDown() {
     var refToMyDiv = document.getElementById("myCar");
     leftMargin = leftMargin + 10;
     refToMyDiv.style.marginLeft = leftMargin + "px";
+    var queueNo = window.setInterval(GearDown, 1000);
+    queueNosArray.push(queueNo);
+    console.log("Gear " + queueNo);
 }
 
-function MarioMoveLeft() {
+function GearUp() {
     var refToMyDiv = document.getElementById("myCar");
     leftMargin = leftMargin - 10;
     refToMyDiv.style.marginLeft = leftMargin + "px";
+    var queueNo = queueNosArray.pop();
+    window.clearInterval(queueNo);
+    console.log("Gear " + queueNo);
+}
+
+
+marginTop =0;
+function MarioMoveDown() {
+    var refToMyDiv = document.getElementById("myCar");
+    marginTop = marginTop + 10;
+    refToMyDiv.style.marginTop = marginTop + "px";
+}
+
+function Respawn(){
+    window.location.href = "Games.html";
 }
